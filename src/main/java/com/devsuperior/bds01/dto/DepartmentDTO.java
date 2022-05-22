f10648182be8d4e7edc9920769ae8770a73a57f0
Fabio Silva <fsilva.developer@gmail.com>
2022-05-22 14:30:23 -0300

    Tests passing

---
+++ src/main/java/com/devsuperior/bds01/dto/DepartmentDTO.java
@@ -0,0 +1,61 @@
+package com.devsuperior.bds01.dto;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.devsuperior.bds01.entities.Department;
+
+public class DepartmentDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+
+	public DepartmentDTO() {
+
+	}
+
+	public DepartmentDTO(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public DepartmentDTO(Department entity) {
+		id = entity.id;
+		name = entity.name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DepartmentDTO other = (DepartmentDTO) obj;
+		return Objects.equals(id, other.id);
+	}
+}
